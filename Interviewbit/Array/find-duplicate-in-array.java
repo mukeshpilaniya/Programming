efc9627429162375319c6d548fed5539040a7d07
Mukesh Pilaniya <pilaniyamukesh0@gmail.com>
2021-01-22 00:35:12 +0530

    Update find-duplicate-in-array.java

--- Interviewbit/Array/find-duplicate-in-array.java
+++ Interviewbit/Array/find-duplicate-in-array.java
@@ -1,7 +1,7 @@
 public class Solution {
     // DO NOT MODIFY THE ARGUMENTS WITH "final" PREFIX. IT IS READ ONLY
     public int repeatedNumber(final int[] A) {
-        HashSet s= new HashSet();
+        HashSet<Integer> s= new HashSet<Integer>();
         
         for(int i=0;i<A.length;i++){
             if(s.contains(A[i])){
